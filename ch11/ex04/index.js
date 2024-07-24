ca89d10f113839fb73e077c7f40a5ac5b23d9a00
iwasakinatsumi <natsumi_iwasaki@jp.ricoh.com>
2024-07-24 22:59:05 +0900

    2024/07/24 実施分

--- ch11/ex04/index.js
+++ ch11/ex04/index.js
@@ -1,5 +1,5 @@
 // これから (N, K) と (K, M) の行列の乗算を行う (この値は色々変更して試すこと)
-const [N, K, M] = [100, 200, 300];
+const [N, K, M] = [1, 2, 3];
 
 // 配列版: (N, K) の行列を要素数 N * K の1次元配列で表現する ((i, j) は array[K * i + j] で参照)
 const lhsA = Array(N * K)
@@ -13,6 +13,10 @@ const resultA = Array(N * M).fill(0.0);
 function arrayMultiply() {
   resultA.fill(0.0);
   // 問題: ここで resultA に lhsA と rhsA の乗算結果を格納してね
+  //うまいことループさせて桁大きくしても大丈夫なようにする
+  resultA[0] = lhsA[0] * rhsA[0] + lhsA[1] * rhsA[1];
+  resultA[1] = lhsA[0] * rhsA[2] + lhsA[1] * rhsA[3];
+  resultA[2] = lhsA[0] * rhsA[4] + lhsA[1] * rhsA[5];
 }
 
 // 型付き配列版 (Float64Array 以外の型も試してみると良い)
@@ -23,6 +27,9 @@ const resultB = new Float64Array(N * M).fill(0.0);
 function typedArrayMultiply() {
   resultB.fill(0.0);
   // 問題: ここで resultB に lhsB と rhsB の乗算結果を格納してね
+  resultB[0] = lhsB[0] * rhsB[0] + lhsB[1] * rhsB[1];
+  resultB[1] = lhsB[0] * rhsB[2] + lhsB[1] * rhsB[3];
+  resultB[2] = lhsB[0] * rhsB[4] + lhsB[1] * rhsB[5];
 }
 
 const TEST_TIMES = 100;
