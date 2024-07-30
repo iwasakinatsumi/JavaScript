c2f41ef8905150b9017eb89ef2b0a3789830bba5
natsumi <natsumi_iwasaku@jp.ricoh.com>
2024-07-30 17:22:10 +0900

    2024/07/30実施分

--- ch11/ex01/index.js
+++ ch11/ex01/index.js
@@ -11,19 +11,28 @@
  * Map と同様のインタフェース(get, set)を持つ。ただし、key はコンストラクタ関数に限定する
  * set では、 コンストラクタ関数の key と そのクラスの value のみ受け付け、それ以外の値が渡された場合はエラーとする。
  * これにより、get で取得する値が key に指定したコンストラクタ関数のクラスであることを保証する。
- * TypeScriptの場合はそのような key, value の型定義とする
+ *  TypeScriptの場合はそのような key, value の型定義とする
  * プリミティブ値は、ラッパークラスのコンストラクタ関数で get/set 可能とする
  */
 
-class TypeMap {
-  constructor(key, value) {
-    this.key = key;
-    this.value = value;
-  }
+//class Foo {}
 
-  set(key, value) {}
-
-  get() {
-    return this.key;
+class TypeMap extends Map {
+  set(key, value) {
+    //valueがkeyの型かどうかチェックする
   }
 }
+
+const typeMap = new TypeMap();
+typeMap.set(String, "string");
+typeMap.set(Number, 123);
+typeMap.set(Foo, new Foo());
+typeMap.set(Date, "not a date"); // -> Error
+console.log(typeMap.get(String)); // -> "string"
+console.log(typeMap.get(Number)); // -> 123
+
+// let myMap = new Map();
+// myMap.set("bar", "foo");
+// myMap.set(1, "foobar");
+// console.log(myMap.get("bar"));
+// console.log(myMap.get(1));
--- ch11/ex04/index.js
+++ ch11/ex04/index.js
@@ -1,5 +1,5 @@
 // これから (N, K) と (K, M) の行列の乗算を行う (この値は色々変更して試すこと)
-const [N, K, M] = [1, 2, 3];
+const [N, K, M] = [10, 20, 30];
 
 // 配列版: (N, K) の行列を要素数 N * K の1次元配列で表現する ((i, j) は array[K * i + j] で参照)
 const lhsA = Array(N * K)
@@ -10,13 +10,26 @@ const rhsA = Array(K * M)
   .map(() => Math.random());
 const resultA = Array(N * M).fill(0.0);
 
+//(N,K) = (1,2)
+//(K,M) =
+// (1,2,3)
+// (4,5,6)
+
 function arrayMultiply() {
   resultA.fill(0.0);
   // 問題: ここで resultA に lhsA と rhsA の乗算結果を格納してね
   //うまいことループさせて桁大きくしても大丈夫なようにする
-  resultA[0] = lhsA[0] * rhsA[0] + lhsA[1] * rhsA[1];
-  resultA[1] = lhsA[0] * rhsA[2] + lhsA[1] * rhsA[3];
-  resultA[2] = lhsA[0] * rhsA[4] + lhsA[1] * rhsA[5];
+  // resultA[0] = lhsA[0] * rhsA[0] + lhsA[1] * rhsA[1];
+  // resultA[1] = lhsA[0] * rhsA[2] + lhsA[1] * rhsA[3];
+  // resultA[2] = lhsA[0] * rhsA[4] + lhsA[1] * rhsA[5];
+
+  for (i = 0; i++; i < M) {
+    for (k = 0; k++; k < K) {
+      for (j = 0; (j = j + 2); j < K * M) {
+        resultA[i] = lhsA[k] * rhsA[j] + lhsA[k + 1] * rhsA[j + 1];
+      }
+    }
+  }
 }
 
 // 型付き配列版 (Float64Array 以外の型も試してみると良い)
@@ -27,9 +40,17 @@ const resultB = new Float64Array(N * M).fill(0.0);
 function typedArrayMultiply() {
   resultB.fill(0.0);
   // 問題: ここで resultB に lhsB と rhsB の乗算結果を格納してね
-  resultB[0] = lhsB[0] * rhsB[0] + lhsB[1] * rhsB[1];
-  resultB[1] = lhsB[0] * rhsB[2] + lhsB[1] * rhsB[3];
-  resultB[2] = lhsB[0] * rhsB[4] + lhsB[1] * rhsB[5];
+  // resultB[0] = lhsB[0] * rhsB[0] + lhsB[1] * rhsB[1];
+  // resultB[1] = lhsB[0] * rhsB[2] + lhsB[1] * rhsB[3];
+  // resultB[2] = lhsB[0] * rhsB[4] + lhsB[1] * rhsB[5];
+
+  for (i = 0; i++; i < M) {
+    for (k = 0; k++; k < K) {
+      for (j = 0; (j = j + 2); j < K * M) {
+        resultB[i] = lhsB[k] * rhsB[j] + lhsB[k + 1] * rhsB[j + 1];
+      }
+    }
+  }
 }
 
 const TEST_TIMES = 100;
