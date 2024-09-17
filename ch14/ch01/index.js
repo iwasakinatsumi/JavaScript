e7353516dc2b899fd26986a31b623dfd804e87d3
natsumi <natsumi_iwasaku@jp.ricoh.com>
2024-09-17 16:45:04 +0900

    関数の定義作成

--- ch14/ch01/index.js
+++ ch14/ch01/index.js
@@ -1,2 +1,9 @@
 //与えられたテストを通過するように、プロパティ、属性を設定したオブジェクトを返す関数を作成しなさい。
 //14.2
+export function unwritableAndUnconfigurableObj() {
+    
+}
+
+export function writableAndUnconfigurableObj() {}
+
+export function nestedUnwritableObj() {}
--- ch14/ch03/index.js
+++ ch14/ch03/index.js
@@ -3,3 +3,4 @@
  * 合成可能なダイアクリティカルマークは文字列を Unicode 正規化して分解し、 \u0300-\u036f の範囲を取り除くと除去できます。
  * 出題範囲: 14.4.6
  */
+export class IgnoreAccentPattern {}
