6abb6b4df92d65b7df53fdad93be6be0fdc938c7
natsumi <natsumi_iwasaku@jp.ricoh.com>
2024-11-06 13:53:31 +0900

    不足ファイルの追加

--- ch15-4/10.6/index.js
+++ ch15-4/10.6/index.js
@@ -1,3 +1,11 @@
+/**
+ * ex06/index.js の続きを実装し Web Components を使った ToDo アプリを完成させなさい。
+ * 補足: この問題では ToDo アプリ全体を 1 つの Web Components (<todo-app>) としたが
+ * TodoMVC の例 のように複数の Web Components に分けて実装する方法も考えられる。
+ * https://github.com/tastejs/todomvc/tree/master/examples/web-components
+ * 出題範囲: 15.6
+ */
+
 const template = document.createElement("template");
 template.innerHTML = `\
 <style>
