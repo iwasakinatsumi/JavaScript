977e9fcd78212b5115a028bd38feb8434e8e5466
iwasakinatsumi <natsumi_iwasaki@jp.ricoh.com>
2025-03-06 22:34:26 +0900

    機能修正

--- clock/script.js
+++ clock/script.js
@@ -3,23 +3,31 @@ dayjs.extend(window.dayjs_plugin_utc); // UTC プラグインを拡張
 dayjs.extend(window.dayjs_plugin_timezone); // タイムゾーンプラグインを拡張
 
 // Google Maps APIのキーをここに入力
-const googleMapsApiKey = "Your google Maps Api Key";
+const googleMapsApiKey = "Your Google Maps Api Key";
 
 let locationTimeInterval = null; // 地点の時刻を更新するインターバルID
 let currentTimeZone = ""; // 現在のタイムゾーン
+
+let marker;
 
 // 現在の時刻を表示 (日本標準時)
 function updateClock() {
   const now = dayjs().tz("Asia/Tokyo"); // 日本標準時
   const formattedTime = now.format("YYYY-MM-DD HH:mm:ss");
-  document.getElementById("clock").textContent = `現在の時刻: ${formattedTime}`;
+  document.getElementById(
+    "clock"
+  ).textContent = `現在の東京の時刻: ${formattedTime}`;
 }
 
 setInterval(updateClock, 1000); // 1秒ごとに更新
 updateClock(); // 初回表示
 
 // 地図の初期設定
-const map = L.map("map").setView([35.6762, 139.6503], 5); // 東京を中央にセット
+let lat = 35.6762;
+let lon = 139.6503;
+const map = L.map("map").setView([lat, lon], 5); // 東京を中央にセット
+
+marker = L.marker([lat, lon]).addTo(map);
 
 // 地図のタイルレイヤー設定 (OpenStreetMap)
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -29,8 +37,20 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 // 地図のクリックイベント
 map.on("click", function (e) {
-  const lat = e.latlng.lat.toFixed(4);
-  const lon = e.latlng.lng.toFixed(4);
+  lat = e.latlng.lat.toFixed(4);
+  lon = e.latlng.lng.toFixed(4);
+
+  //マーカーの指定があれば消す
+  if (marker) {
+    map.removeLayer(marker);
+  }
+
+  //ピン表示
+  marker = L.marker([lat, lon]).addTo(map);
+
+  // 地点の時刻を初期化して空にする
+  document.getElementById("location").textContent = "";
+
   getTimeZone(lat, lon);
 });
 
@@ -51,7 +71,7 @@ function getTimeZone(lat, lon) {
         if (locationTimeInterval) {
           clearInterval(locationTimeInterval); // 前のインターバルをクリア
         }
-        locationTimeInterval = setInterval(updateLocationTime, 1000); // 地点の時刻を更新
+        locationTimeInterval = setInterval(() => updateLocationTime(), 1000); // 地点の時刻を更新
       } else {
         document.getElementById("location").textContent =
           "タイムゾーンの取得に失敗しました";
@@ -70,11 +90,14 @@ function updateLocationTime() {
     const locationTime = dayjs()
       .tz(currentTimeZone)
       .format("YYYY-MM-DD HH:mm:ss");
-    document.getElementById(
-      "location"
-    ).textContent = `地点の現在の時刻: ${locationTime} (${currentTimeZone})`;
+
+    if (lat && lon) {
+      document.getElementById(
+        "location"
+      ).textContent = `緯度: ${lat}, 経度: ${lon} 地点の現在の時刻: ${locationTime} (${currentTimeZone})`;
+    } else {
+      document.getElementById("location").textContent =
+        "緯度・経度の取得に失敗しました";
+    }
   }
 }
-
-// ズームコントロールの表示
-L.control.zoom().addTo(map);
